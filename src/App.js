5cf2c1255a63a7606243133dc6e1c34df552f1c3
KanoCode <kanokanombola@gmail.com>
2022-09-23 00:53:54 +0200

    add redux store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { Routes, Route, Link } from 'react-router-dom';
+import Greeting from './Greeting';
+import Home from './Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/greeting">greeting</Link>
+        <Link to="/">home</Link>
+      </nav>
+      <Routes>
+        <Route path="/greeting" element={<Greeting />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      Home
+      <h2>Hello World </h2>
+      <p>This is the homepage</p>
+    </div>
+  );
+}
